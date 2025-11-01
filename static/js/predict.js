20ff0b34bdd732eadd225fa5afb359e2e7d63a18
SaideepikaLakkireddy <saideepika2502@gmail.com>
2025-11-01 23:03:36 +0530

    Updated API_BASE to Render URL

--- static/js/predict.js
+++ static/js/predict.js
@@ -1,5 +1,5 @@
 // ==================== BASE URL ====================
-const API_BASE = "http://127.0.0.1:5000"; 
+const API_BASE = "https://event-attendance-prediction-1-0z9g.onrender.com/"; 
 
 // ==================== EVENT FORM LOGIC ====================
 document.addEventListener("DOMContentLoaded", (e) => {
